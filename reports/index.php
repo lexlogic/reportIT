ac63ef6ef7e93f9dcb2d511c841d8e21b818619d
nickmc01 <nickmc01@gmail.com>
2014-02-22 11:17:44 -0500

    Added report Structure

--- reports/index.php
+++ reports/index.php
@@ -8,22 +8,27 @@ if($user->isLoggedIn() && $user->hasPermission('manager')) {
         echo "<pre>";
         var_dump($_POST);
     }
-
-
     $page = new Page;
     $page->setTitle('Report Generator');
     $page->startBody();
     ?>
-
     <html>
     <body>
     <style>
         #editor{
             width: 100%;
         }
+        .draggable {
+            padding: 5px; float: left; margin: 0 10px 10px 0; font-size: .9em;
+            cursor: hand;
+            z-index: 1000;
+        }
+        .ui-widget-header p, .ui-widget-content p { margin: 0; }
+        #snaptarget { height: 140px; }
     </style>
-
-    <script type="text/javascript" src="jquery-2.1.0.js"></script>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/jquery-1.9.1.js"></script>
+    <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
     <script type="text/javascript" src="tinymce/js/tinymce/tinymce.min.js"></script>
     <script type="text/javascript">
         tinymce.init({
@@ -37,7 +42,23 @@ if($user->isLoggedIn() && $user->hasPermission('manager')) {
             id: "template_editor"
         });
     </script>
-
+    <script>
+        $(function() {
+            $( "#draggable1" ).draggable({ grid: [ 1, 1 ] });
+            $( "#draggable2" ).draggable({ grid: [ 1, 1 ] });
+            $( "#draggable3" ).draggable({ grid: [ 1, 1 ] });
+            $( "#draggable4" ).draggable({ grid: [ 1, 1 ] });
+            $( "#draggable5" ).draggable({ grid: [ 1, 1 ] });
+            $( "#draggable6" ).draggable({ grid: [ 1, 1 ] });
+            $( "#draggable7" ).draggable({ grid: [ 1, 1 ] });
+            $( "#draggable8" ).draggable({ grid: [ 1, 1 ] });
+            $( "#draggable9" ).draggable({ grid: [ 1, 1 ] });
+            $( "#draggable10" ).draggable({ grid: [ 1, 1 ] });
+            $( "#draggable11" ).draggable({ grid: [ 1, 1 ] });
+            $( "#draggable12" ).draggable({ grid: [ 1, 1 ] });
+            $( "#draggable13" ).draggable({ grid: [ 1, 1 ] });
+        });
+    </script>
     <div class="col-lg-6">
         <div class="block-flat">
             <div class="header">
@@ -45,17 +66,16 @@ if($user->isLoggedIn() && $user->hasPermission('manager')) {
             </div>
             <div class="content">
                 <div id="editor">
-                    <form method="post" enctype="multipart/form-data" action="">
+                    <form method="post" action="template_update.php">
                         <textarea name="content" style="width:100%"></textarea>
                         <div class="form-group">
-                            <input type="submit" class="btn btn-info" name="uploadfile" value="Submit">
+                            <input type="submit" class="btn btn-info" value="Submit">
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-
     <div class="col-lg-6">
         <div class="block-flat">
             <div class="header">
@@ -63,130 +83,145 @@ if($user->isLoggedIn() && $user->hasPermission('manager')) {
             </div>
             <div class="content">
                 Findings are based on the DREAD model.<br>
-                ${FINDING_NAME} -> Finding Name <br>
-                ${CRITICAL_STATUS} -> Critical Status IE: Exploited, Confirmed<br>
-                ${RISK_RATE} -> Risk Rating: Critical, Severe, Moderate, Low. Based on score<br>
-                ${CRI_DAM} -> Damage Potential <br>
-                ${CRI_REPRO} -> Reproducibility<br>
-                ${CRI_EXP} ->  Exploitability <br>
-                ${CRI_AFEC} -> Affected Users<br>
-                ${CRI_DIS} -> Discoverability <br>
-                ${CRI_TTL} ->  Total<br>
-                ${CRI_REM} -> Remediation Effort <br>
-                ${CRITICAL_SUMMARY} -> Finding Summary <br>
-                ${CRITICAL_PROOF} -> Finding Proof <br>
-                ${CRITICAL_RECOMMENDATIONS} -> Finding Recommendations <br>
+                <div id="draggable1" class="draggable ui-widget-content">${FINDING_NAME}</div>
+                -> Finding Name<br><br><br>
+
+                <div id="draggable2" class="draggable ui-widget-content">${CRITICAL_STATUS}</div>
+                -> Critical Status IE: Exploited, Confirmed<br><br><br>
+
+
+                <div id="draggable3" class="draggable ui-widget-content">${RISK_RATE}</div>
+                -> Risk Rating: Critical, Severe, Moderate, Low. Based on score<br><br><br>
+
+
+                <div id="draggable4" class="draggable ui-widget-content">${CRI_DAM}</div>
+                -> Damage Potential<br><br><br>
+
+
+                <div id="draggable5" class="draggable ui-widget-content">${CRI_REPRO}</div>
+                -> Reproducibility<br><br><br>
+
+
+                <div id="draggable6" class="draggable ui-widget-content">${CRI_EXP}</div>
+                ->  Exploitability <br><br><br>
+
+
+                <div id="draggable7" class="draggable ui-widget-content">${CRI_AFEC}</div>
+                -> Affected Users<br><br><br>
+
+
+                <div id="draggable8" class="draggable ui-widget-content">${CRI_DIS}</div>
+                -> Discoverability <br><br><br>
+
+
+                <div id="draggable9" class="draggable ui-widget-content">${CRI_TTL}</div>
+                ->  Total<br><br><br>
+
+
+                <div id="draggable10" class="draggable ui-widget-content">${CRI_REM}</div>
+                -> Remediation Effort <br><br><br>
+
+
+                <div id="draggable11" class="draggable ui-widget-content">${CRITICAL_SUMMARY}</div>
+                -> Finding Summary <br><br><br>
+
+
+                <div id="draggable12" class="draggable ui-widget-content">${CRITICAL_PROOF}</div>
+                -> Finding Proof <br><br><br>
+
+
+                <div id="draggable13" class="draggable ui-widget-content">${CRITICAL_RECOMMENDATIONS}</div>
+                -> Finding Recommendations <br><br><br>
             </div>
         </div>
     </div>
+
 
 
     <div class="col-lg-12 pull-left">
         <div class="block-flat">
             <div class="header">
-                <h3>${FINDING_NAME}</h3>
-            </div>
-            <div class="content">
-                <div class="table-responsive">
-                    <table class="table table-bordered">
-                        <thead>
-                        <tr>
-                            <div style="float: left;">Dread Score Summary: ${CRI_DAM}<br><br>Critical Status: ${CRITICAL_STATUS}<br></div>
-                        </tr>
-                        <tr>
-                            <th>Risk Rating</th>
-                            <th>Damage Potential</th>
-                            <th>Reproducibility</th>
-                            <th>Exploitability</th>
-                            <th>Affected Users</th>
-                            <th>Discoverability</th>
-                            <th>Total</th>
-                            <th>Remediation Effort</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td>
-                                ${RISK_RATE}
-                            </td>
-                            <td>
-                                ${CRI_DAM}
-                            </td>
-                            <td>
-                                ${CRI_REPRO}
-                            </td>
-                            <td>
-                                ${CRI_EXP}
-                            </td>
-                            <td>
-                                ${CRI_AFEC}
-                            </td>
-                            <td>
-                                ${CRI_DIS}
-                            </td>
-                            <td>
-                                ${CRI_TTL}
-                            </td>
-                            <td>
-                                ${CRI_REM}
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
-                    <table class="table table-bordered">
-                        <thead>
-                        <tr>
-                            <th>Summary of Finding</th>
-                            <th>Proof of Concept</th>
-                            <th>Recommendations</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td>
-                                ${CRITICAL_SUMMARY}
-                            </td>
-                            <td>
-                                ${CRITICAL_PROOF}
-                            </td>
-                            <td>
-                                ${CRITICAL_RECOMMENDATIONS}
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
+                ${FINDING_NAME}<br>
+                <strong>Dread Score Summary&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; STATUS: </strong>${CRITICAL_STATUS}<br>
+                <table>
+                    <tbody>
+                    <tr>
+                        <td>
+                            <table>
+                                <tbody>
+                                <tr>
+                                    <td colspan='4'>
+                                        <strong>Risk Rating</strong><br>
+                                    </td>
+                                    <td colspan='4'>
+                                        <p style="text-align: right;">${RISK_RATE}</p>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        Damage<br>
+                                        Potential<br>
+                                    </td>
+                                    <td>
+                                        Reproducibility<br>
+                                    </td>
+                                    <td>
+                                        Exploitability<br>
+                                    </td>
+                                    <td colspan='2'>
+                                        Affected Users<br>
+                                    </td>
+                                    <td>
+                                        Discoverability<br>
+                                    </td>
+                                    <td>
+                                        <p>Total</p>
+                                    </td>
+                                    <td>
+                                        Remediation<br>
+                                        Effort<br>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        ${CRI_DAM}<br>
+                                    </td>
+                                    <td>
+                                        ${CRI_REPRO}<br>
+                                    </td>
+                                    <td>
+                                        ${CRI_EXP}<br>
+                                    </td>
+                                    <td colspan='2'>
+                                        ${CRI_AFEC}<br>
+                                    </td>
+                                    <td>
+                                        ${CRI_DIS}<br>
+                                    </td>
+                                    <td>
+                                        ${CRI_TTL}<br>
+                                    </td>
+                                    <td>
+                                        ${CRI_REM}<br>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <strong>Summary of Finding</strong><br>
+                ${CRITICAL_SUMMARY}<br>
+                <strong>Proof of Concept</strong><br>
+                {CRITICAL_PROOF}<br>
+                <strong>Recommendations</strong><br>
+                ${CRITICAL_RECOMMENDATIONS}<br>
             </div>
         </div>
     </div>
-
-    <script>
-        function alertAllEditorIDs () {
-            var IDs = new Array();
-            var editorID;
-            for (editorID in tinyMCE.editors) {
-                IDs[IDs.length] = editorID;
-            };
-
-            alert("All editor IDs:\n" + IDs);
-        }
-        function save(){
-            var st = $('content');
-            $.ajax({
-                type: "POST",
-                url: "index.php",
-                data: {data: st},
-                sucess: function (returndata) {
-                    alert( "Data Loaded: " + returndata );
-                    console.log(returndata);
-                    checkstatus(data);
-                }
-            });
-        }
-    </script>
     </body>
     </html>
-
     <?php
     $page->endBody();
     echo $page->render('../includes/template.php');
--- reports/template_update.php
+++ reports/template_update.php
@@ -1,13 +1,77 @@
-<php
-$con=mysqli_connect("localhost","root","","reportify");
-// Check connection
-if (mysqli_connect_errno($con))
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-print_r $_POST[];
+<?php
+include_once('PHPWord/Autoloader.php');
+include_once('FirePHPCore/fb.php');
+require_once '../Init.php';
 
 
 
+$incomming = $_POST;
+$html_code= $_POST['data'];
 
+$update = DB::getInstance()->update('template', '1', array(
+    'htmlcode' => $html_code
+));
+
+$PHPWord = new PHPWord();
+$section = $PHPWord->createSection();
+
+$html = $html_code;
+
+// HTML Dom object:
+$html_dom = new simple_html_dom();
+$html_dom->load('<html><body>' . $html . '</body></html>');
+// Note, we needed to nest the html in a couple of dummy elements.
+fb($html_dom);
+// Create the dom array of elements which we are going to work on:
+$html_dom_array = $html_dom->find('html',0)->children();
+
+// Provide some initial settings:
+$initial_state = array(
+    // Required parameters:
+    'phpword_object' => &$phpword_object, // Must be passed by reference.
+    'base_root' => 'http://test.local', // Required for link elements - change it to your domain.
+    'base_path' => '/htmltodocx/', // Path from base_root to whatever url your links are relative to.
+
+    // Optional parameters - showing the defaults if you don't set anything:
+    'current_style' => array('size' => '11'), // The PHPWord style on the top element - may be inherited by descendent elements.
+    'parents' => array(0 => 'body'), // Our parent is body.
+    'list_depth' => 0, // This is the current depth of any current list.
+    'context' => 'section', // Possible values - section, footer or header.
+    'pseudo_list' => TRUE, // NOTE: Word lists not yet supported (TRUE is the only option at present).
+    'pseudo_list_indicator_font_name' => 'Wingdings', // Bullet indicator font.
+    'pseudo_list_indicator_font_size' => '7', // Bullet indicator size.
+    'pseudo_list_indicator_character' => 'l ', // Gives a circle bullet point with wingdings.
+    'table_allowed' => TRUE, // Note, if you are adding this html into a PHPWord table you should set this to FALSE: tables cannot be nested in PHPWord.
+    'treat_div_as_paragraph' => TRUE, // If set to TRUE, each new div will trigger a new line in the Word document.
+
+    // Optional - no default:
+    'style_sheet' => htmltodocx_styles_example(), // This is an array (the "style sheet") - returned by htmltodocx_styles_example() here (in styles.inc) - see this function for an example of how to construct this array.
+);
+
+// Convert the HTML and put it into the PHPWord object
+htmltodocx_insert_html($section, $html_dom_array[0]->nodes, $initial_state);
+
+// Clear the HTML dom object:
+$html_dom->clear();
+unset($html_dom);
+
+// Save File
+$h2d_file_uri = tempnam('', 'htd');
+$objWriter = PHPWord_IOFactory::createWriter($phpword_object, 'Word2007');
+$objWriter->save($h2d_file_uri);
+
+// Download the file:
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename=example.docx');
+header('Content-Transfer-Encoding: binary');
+header('Expires: 0');
+header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+header('Pragma: public');
+header('Content-Length: ' . filesize($h2d_file_uri));
+ob_clean();
+flush();
+$status = readfile($h2d_file_uri);
+unlink($h2d_file_uri);
+exit;
 ?>
